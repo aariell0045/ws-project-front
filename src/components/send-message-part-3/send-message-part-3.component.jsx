c5d6a39f058d25fc59b399e23c736945318c1c55
toam <toamcohen10@gmail.com>
2021-09-08 01:30:11 +0300

    adding folder send-message-part-3

---
+++ src/components/send-message-part-3/send-message-part-3.component.jsx
@@ -0,0 +1,45 @@
+import "./send-message-part-3.styles.css";
+import React from "react";
+import PhoneSvg from "../../phone.svg";
+
+function SendMessagePart3() {
+	return (
+		<section id='send-message-part3'>
+			<div className='send-message-part3-warpper last-warpper-1'>
+				<img className='phoen-svg' src={PhoneSvg} alt='React Logo' />
+				<div className='content-phone-box'>red</div>
+			</div>
+			<div className='send-message-part3-warpper'>
+				<div className='send-message-part3-warpper-2' >
+				<header className='send-message-part3-sub-header'>
+
+					<p>ההודעה תשלח לקבוצות הבאות:</p>
+				</header>
+
+				<div className='send-message-part3-group-name-container'>שם הקבוצה</div>
+				</div>
+
+				<header className='send-message-part3-sub-header'>
+					<p>תנאי שימוש:</p>
+				</header>
+				<div className='send-message-part3-conditions-container'>
+					<article className='send-message-part3-conditions-container-content' >
+						<p>
+
+					כל יהיו כתובים כל תנאי השימוש כל יהיו כתובים כל תנאי השימוש כל יהיו כתובים כל תנאי השימוש כל יהיו כתובים כל תנאי השימוש כל יהיו כתובים כל תנאי השימוש כל יהיו כתובים כל תנאי השימוש כל יהיו כתובים כל תנאי השימוש כל יהיו כתובים כל תנאי השימוש כל יהיו כתובים כל תנאי השימוש כל יהיו כתובים כל תנאי השימושכל יהיו כתובים כל תנאי השימוש כל יהיו כתובים כל תנאי השימוש כל יהיו כתובים כל תנאי השימוש כל יהיו כתובים כל תנאי השימוש כל יהיו כתובים כל תנאי השימושכל יהיו כתובים כל תנאי השימוש כל יהיו כתובים כל תנאי השימוש כל יהיו כתובים כל תנאי השימוש כל יהיו כתובים כל תנאי השימוש כל יהיו כתובים כל תנאי השימוש
+					</p>
+					</article>
+					<div className='i-agree-line'>
+						<input type="checkbox" />
+						אני מאשר/ת שקראתי ואני מסכימ/ה לכל תנאי השימוש.
+					</div>
+				</div>
+
+				
+			</div>
+			<div className='send-message-part3-warpper'></div>
+		</section>
+	);
+}
+
+export default SendMessagePart3;
