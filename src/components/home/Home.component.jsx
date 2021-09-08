f66acd94a6eb67390f7dfe4ee26ab09999dbf200
toam <toamcohen10@gmail.com>
2021-09-08 04:27:03 +0300

    and a new home page component template

--- src/components/home/Home.component.jsx
+++ src/components/home/Home.component.jsx
@@ -1,69 +1,103 @@
-import React from 'react';
-import Clock from '../clock/Clock.component';
-import "./home.styles.css"
-
-const weekDays = ["א'", "ב'", "ג'", "ד'", "ה'", "ו'","ש'"];
-let mounth = [];
+import React from "react";
+import "./home.styles.css";
 
 function Home() {
 	return (
-		<section id='home-page' >
-			<div className='home-page-header' >
-				<div className='search-plus-header-warper' >
-					<h2 className='username-header' >משפט מגניב, <span>עומר!</span></h2>
-					<input className='fast-search-input' type="text" placeholder="חיפוש מהיר" />
+		<section id='home-page'>
+			<div className='home-page-warpper'>
+				<div className='home-page-left-side'>
+					<header className='home-page-left-side-main-header'>
+						<p className='home-page-left-side-header-content'>
+							ערב טוב,{" "}
+							<span className='home-page-left-side-header-content-username-name'>
+								ישראל
+							</span>
+							!
+						</p>
+						<input
+							className='home-page-left-side-main-input'
+							type='text'
+							placeholder={`חיפוש מהיר, )לדוג' שם קבוצה או שם הודעה..(`}
+						/>
+					</header>
+
+					<div className='my-tasks-container'>
+						<div className='my-tasks-main-header'>
+							<p className='my-tasks-header-content'>המשימות שלי</p>
+						</div>
+						<div className='my-tasks-list'>
+							<div className='task-box-warpper'>
+								<input type='radio' />
+								<div className='task-box'>
+									<p className='importance-level'></p> שם המשימה
+								</div>
+							</div>
+							<div className='task-box-warpper'>
+								<input type='radio' />
+								<div className='task-box'>
+									<p className='importance-level'></p> שם המשימה
+								</div>
+							</div>
+							<div className='task-box-warpper'>
+								<input type='radio' />
+								<div className='task-box'>
+									<p className='importance-level'></p> שם המשימה
+								</div>
+							</div>
+						</div>
+						<div className='my-task-button-warpper'>
+							<button className='add-new-task-button'>משימה חדשה</button>
+						</div>
+					</div>
 				</div>
-				<Clock />
+				<div className='home-page-right-side'>
+					<div className='calander'>
+						<div className='calander-date'>
+							<p className='home-page-left-arrow'>left</p>
+							<p className='home-page-date-content'>פברואר 2021</p>
+							<p classNames='home-page-left-arrow'>right</p>
+						</div>
+						<div className='week-days-warpper'>
+							<div className='week-days'>
+								<p className='day'>א'</p>
+								<p className='day'>ב'</p>
+								<p className='day'>ג'</p>
+								<p className='day'>ד'</p>
+								<p className='day'>ה'</p>
+								<p className='day'>ו'</p>
+								<p className='day'>ש'</p>
+							</div>
+						</div>
+						<div className='dates-list'></div>
+					</div>
+					<div className='home-page-events'>
+						<div className='calander-date'>
+							<p className='home-page-left-arrow'>left</p>
+							<p className='home-page-date-content'>פברואר 2021</p>
+							<p classNames='home-page-left-arrow'>right</p>
+						</div>
+						<div className='my-tasks-list'>
+							<div className='task-box-warpper'>
+								
+								<div className='task-box'>
+									<p className='importance-level'></p> שם האירוע</div>
+							</div>
+							<div className='task-box-warpper'>
+								
+								<div className='task-box'>
+									<p className='importance-level'></p> שם האירוע</div>
+							</div>
+							<div className='task-box-warpper'>  
+								
+								<div className='task-box'>
+									<p className='importance-level'></p> שם האירוע</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
-
-			<main id='home-main-content'>
-				<div className='main-content-right-side' >
-						<div className='container-1' >
-							<div className='container-1-1' >
-								<h3 className='my-tasks-headers' >המשימות שלי</h3>
-								<button className='my-tasks-button' >הוספת משימה</button>
-							</div>
-						</div>
-						<div className='container-3' >
-						<div className='container-3-3' >
-							<h3 className='recently-preformed-header' >בוצע לאחרונה</h3>
-							<span className='recently-preformed-link' >הצג הכל</span>
-						</div>
-					</div>
-				</div>
-				<div className='main-content-left-side' >
-					<div className='container-2' >
-						<div className='container-2-2'>
-								<span>right</span>
-								<span>{new Date().toLocaleDateString()}</span>
-								<span>left</span>
-						</div>
-						<div className='container-4' >
-							<div className='container-shadow-4-4' ></div>
-
-							<div className='container-4-4'>
-									<span>יום שישי, 2021/2/15</span>
-							</div>
-							<div className='container-4-4-main-content' >
-
-							</div>
-							<div className='container-4-footer'>
-								<button className='event-button' >הוספת אירוע</button>
-							</div>
-						</div>
-					</div>
-					
-				</div>
-
-			
-					
-			</main>
-
-
-		
 		</section>
-	)
-
+	);
 }
 
-export default Home
+export default Home;
