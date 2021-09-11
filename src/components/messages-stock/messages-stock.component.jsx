568a3f0d9f6059f3008202573a5fbea9da7db0c2
toam <toamcohen10@gmail.com>
2021-09-12 02:36:10 +0300

    didnt work on add message just work on the icon and styling

--- src/components/messages-stock/messages-stock.component.jsx
+++ src/components/messages-stock/messages-stock.component.jsx
@@ -2,91 +2,99 @@ import "./messages-stock.styles.css";
 import React, { useState } from "react";
 import MessageBox from "../message-box/message-box.component";
 import AddMessage from "../add-message/add-message.component";
-
+import AddMessageIcon from "../../icons/icons-components/add-message/add-message.component";
 
 const messages = [
-  { messageName: "ariel", contentMessage: "שלום מה קורה אחשלי היקר מכל!!!!!" },
-  { messageName: "ariel", contentMessage: "שלום מה קורה אחשלי היקר מכל!!!!!" },
-  { messageName: "toam", contentMessage: "שלום מה קורה אחשלי היקר מכל!!!!!" },
-  { messageName: "toam", contentMessage: "שלום מה קורה אחשלי היקר מכל!!!!!" },
+	{ messageName: "ariel", contentMessage: "שלום מה קורה אחשלי היקר מכל!!!!!" },
+	{ messageName: "ariel", contentMessage: "שלום מה קורה אחשלי היקר מכל!!!!!" },
+	{ messageName: "toam", contentMessage: "שלום מה קורה אחשלי היקר מכל!!!!!" },
+	{ messageName: "toam", contentMessage: "שלום מה קורה אחשלי היקר מכל!!!!!" },
 ];
 
 function MessagesStock() {
-  const [createMessage, setCreateMessage] = useState(false);
-  const [messagesList, setMessageList] = useState(messages);
-  const [state, setState] = useState({
-    searchMessages: "",
-  });
+	const [createMessage, setCreateMessage] = useState(false);
+	const [messagesList, setMessageList] = useState(messages);
+	const [state, setState] = useState({
+		searchMessages: "",
+	});
 
-  function handleInputs({ target }) {
-    const { name, value } = target;
-    let currentState = { ...state };
-    currentState[name] = value;
-    setState(currentState);
-  }
+	function handleInputs({ target }) {
+		const { name, value } = target;
+		let currentState = { ...state };
+		currentState[name] = value;
+		setState(currentState);
+	}
 
-  return (
-    <section id="messages-stock">
-      <div className="messages-stock-warpper">
-        <div className="messages-stock-left-side">
-          <header className="messages-stock-left-side-header">
-            <p>מאגר הודעות</p>
-            <button className="messages-stock-right-side-button-add-new-message">
-              icon
-            </button>
-          </header>
-          <input
-            name="searchMessages"
-            className="messages-stcok-left-side-input"
-            type="text"
-            placeholder="חיפוש במאגר ההודעות"
-            onChange={(event) => handleInputs(event)}
-          />
+	return (
+		<section id='messages-stock'>
+			<div className='messages-stock-warpper'>
+				<div className='messages-stock-left-side'>
+					<header className='messages-stock-left-side-header'>
+						<p>מאגר הודעות</p>
+						<button
+							onClick={(event) => setCreateMessage(!createMessage)}
+							className='messages-stock-right-side-button-add-new-message'
+						>
+							<AddMessageIcon />
+						</button>
+					</header>
+					<input
+						name='searchMessages'
+						className='messages-stcok-left-side-input'
+						type='text'
+						placeholder='חיפוש במאגר ההודעות'
+						onChange={(event) => handleInputs(event)}
+					/>
 
-          <div className="messages-stcok-list">
-            {state.searchMessages &&
-              messagesList.map((message, index) => {
-                if (message.messageName.includes(state.searchMessages)) {
-                  return (
-                    <MessageBox
-                      key={index}
-                      item={[message.messageName, message.contentMessage]}
-                    />
-                  );
-                } else {
-                  return null;
-                }
-              })}
-            {!state.searchMessages &&
-              messagesList.map((message, index) => {
-                return (
-                  <MessageBox
-                    key={index}
-                    item={[message.messageName, message.contentMessage]}
-                  />
-                );
-              })}
-          </div>
-        </div>
-        <div className="messages-stock-right-side">
-          <div className="message-stock-right-side-background"></div>
-          <div className="messsages-stock-tools">
-            <span
-              onClick={(event) => setCreateMessage(!createMessage)}
-              className={
-                createMessage
-                  ? "new-message-tool new-message-tool-active"
-                  : "new-message-tool"
-              }
-            >
-              {"icon"}
-            </span>
-          </div>
-          <div>{createMessage && <AddMessage />}</div>
-        </div>
-      </div>
-    </section>
-  );
+					<div className='messages-stcok-list'>
+						{state.searchMessages &&
+							messagesList.map((message, index) => {
+								if (message.messageName.includes(state.searchMessages)) {
+									return (
+										<MessageBox
+											key={index}
+											item={[
+												message.messageName,
+												message.contentMessage,
+											]}
+										/>
+									);
+								} else {
+									return null;
+								}
+							})}
+						{!state.searchMessages &&
+							messagesList.map((message, index) => {
+								return (
+									<MessageBox
+										key={index}
+										item={[
+											message.messageName,
+											message.contentMessage,
+										]}
+									/>
+								);
+							})}
+					</div>
+				</div>
+				<div className='messages-stock-right-side'>
+					<div className='message-stock-right-side-background'></div>
+					<div className='messsages-stock-tools'>
+						<span
+							className={
+								createMessage
+									? "new-message-tool new-message-tool-active"
+									: "new-message-tool"
+							}
+						>
+							{"icon"}
+						</span>
+					</div>
+					<div>{createMessage && <AddMessage />}</div>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default MessagesStock;
--- src/icons/icons-components/add-message/add-message.component.jsx
+++ src/icons/icons-components/add-message/add-message.component.jsx
@@ -1,87 +1,39 @@
-function addMessageIcon() {
-  return (
-    <svg
-      width="62"
-      height="62"
-      viewBox="0 0 62 62"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g filter="url(#filter0_d)">
-        <circle
-          cx="31"
-          cy="27"
-          r="27"
-          fill="white"
-          fill-opacity="0.8"
-          shape-rendering="crispEdges"
-        />
-        <circle
-          cx="31"
-          cy="27"
-          r="26.5"
-          stroke="black"
-          shape-rendering="crispEdges"
-        />
-      </g>
-      <path
-        d="M38.5005 21.9999C39.9583 26.3731 37.0004 28.4999 39.5004 31.4999C31.9856 35.4405 22.9125 33.6389 23.0003 29.9999C23.0882 26.361 28.7454 22.9553 31.6092 23.8488C34.4731 24.7423 37.5005 18.9999 38.5005 21.9999Z"
-        fill="white"
-      />
-      <rect x="23" y="21" width="16" height="12" rx="2" stroke="black" />
-      <path
-        d="M23 24L30.1056 27.5528C30.6686 27.8343 31.3314 27.8343 31.8944 27.5528L39 24"
-        stroke="black"
-      />
-      <path
-        d="M41.167 31.6665L41.167 38.6665"
-        stroke="black"
-        stroke-linecap="round"
-      />
-      <path
-        d="M44.667 35.1665L37.667 35.1665"
-        stroke="black"
-        stroke-linecap="round"
-      />
-      <defs>
-        <filter
-          id="filter0_d"
-          x="0"
-          y="0"
-          width="62"
-          height="62"
-          filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
-        >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
-          <feColorMatrix
-            in="SourceAlpha"
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-            result="hardAlpha"
-          />
-          <feOffset dy="4" />
-          <feGaussianBlur stdDeviation="2" />
-          <feComposite in2="hardAlpha" operator="out" />
-          <feColorMatrix
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-          />
-          <feBlend
-            mode="normal"
-            in2="BackgroundImageFix"
-            result="effect1_dropShadow"
-          />
-          <feBlend
-            mode="normal"
-            in="SourceGraphic"
-            in2="effect1_dropShadow"
-            result="shape"
-          />
-        </filter>
-      </defs>
-    </svg>
-  );
+function AddMessageIcon() {
+	return (
+		<svg
+			style={{
+				width: "100%",
+				position: "absolute",
+				top: "50%",
+				marginTop: "-0.4vw",
+				left: "50%",
+				marginLeft: "-1.4vw",
+			}}
+			viewBox='0 0 28 20'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'
+		>
+			<svg
+				width='24'
+				height='24'
+				viewBox='8 5 8 25'
+				fill='none'
+				xmlns='http://www.w3.org/2000/svg'
+			>
+				<path
+					d='M19.5 6.99993C20.9578 11.3731 17.9999 13.4999 20.4999 16.4999C12.9852 20.4405 3.91203 18.6389 3.99985 14.9999C4.08768 11.361 9.7449 7.95527 12.6087 8.8488C15.4726 9.74234 18.5 3.99991 19.5 6.99993Z'
+					fill='white'
+				/>
+				<rect x='4' y='6' width='16' height='12' rx='2' stroke='black' />
+				<path
+					d='M4 9L11.1056 12.5528C11.6686 12.8343 12.3314 12.8343 12.8944 12.5528L20 9'
+					stroke='black'
+				/>
+			</svg>
+			<path d='M22.1665 11.6665L22.1665 18.6665' stroke='black' stroke-linecap='round' />
+			<path d='M25.6665 15.1665L18.6665 15.1665' stroke='black' stroke-linecap='round' />
+		</svg>
+	);
 }
 
-export default addMessageIcon
+export default AddMessageIcon;
