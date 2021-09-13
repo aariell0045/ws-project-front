5d0505f2bf6e1fd3d02e7efb1b4878bdfdc59e06
toam <toamcohen10@gmail.com>
2021-09-13 03:05:07 +0300

    add messages styling functionality

--- src/icons/icons-components/trash-icon/trash-icon.component.jsx
+++ src/icons/icons-components/trash-icon/trash-icon.component.jsx
@@ -5,8 +5,8 @@ function TrashIcon(props) {
 	if (disabled) {
 		return (
 			<svg
-				width='21'
-				height='21'
+				width='100%'
+				height='100%'
 				viewBox='0 0 21 21'
 				fill='none'
 				xmlns='http://www.w3.org/2000/svg'
