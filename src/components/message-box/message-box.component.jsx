1c9688b0f556913dbe8865d93bd439b8061de70f
toam <toamcohen10@gmail.com>
2021-09-14 02:33:31 +0300

    finish add message functionalty for adding

--- src/components/message-box/message-box.component.jsx
+++ src/components/message-box/message-box.component.jsx
@@ -2,20 +2,20 @@ import "./message-box.styles.css";
 import React from "react";
 
 function MessageBox(props) {
-  let [messageName, contentMessage] = props.item;
-
-  return (
-    <div className="message-box">
-      <header className="message-box-header">
-        <h3>{messageName}</h3>
-        <p>3 הודעות ברצף</p>
-      </header>
-      <div className="content">
-        <p>{contentMessage}</p>
-        <div className="media-footer">media</div>
-      </div>
-    </div>
-  );
+	const { item } = props;
+	console.log(props.item);
+	return (
+		<div className='message-box'>
+			<header className='message-box-header'>
+				<h3>{item[0]}</h3>
+				<p>3 הודעות ברצף</p>
+			</header>
+			<div className='content'>
+				<p>{}</p>
+				<div className='media-footer'>media</div>
+			</div>
+		</div>
+	);
 }
 
 export default MessageBox;
