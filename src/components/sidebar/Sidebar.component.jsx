581f0d71e460c18c0d0c3538ecb2c4779cd51779
toam <toamcohen10@gmail.com>
2021-12-04 09:33:55 +0200

    Faild-prop-type-fixed

--- src/components/sidebar/Sidebar.component.jsx
+++ src/components/sidebar/Sidebar.component.jsx
@@ -15,79 +15,79 @@ import { addUserId } from "../index.redux/actions/actions";
 import { style } from "@mui/system";
 
 const links = [
-  { linkName: "בית", goTo: "/Home", className: "list list-1" },
-  { linkName: "הודעות", goTo: "/MessagesStock", className: "list list-2" },
-  { linkName: "קבוצות", goTo: "/Groups", className: "list list-3" },
-  { linkName: "שליחה", goTo: "/SendMessagePart1", className: "list list-4" },
-  { linkName: "היסטוריה", goTo: "/HistoryList", className: "list list-5" },
-  { linkName: "פרופיל", goTo: "/UserProfile", className: "list list-6" },
-  { linkName: "התנתקות", goTo: "/SignOut", className: "list list-7" },
+	{ linkName: "בית", goTo: "/Home", className: "list list-1" },
+	{ linkName: "הודעות", goTo: "/MessagesStock", className: "list list-2" },
+	{ linkName: "קבוצות", goTo: "/Groups", className: "list list-3" },
+	{ linkName: "שליחה", goTo: "/SendMessagePart1", className: "list list-4" },
+	{ linkName: "היסטוריה", goTo: "/HistoryList", className: "list list-5" },
+	{ linkName: "פרופיל", goTo: "/UserProfile", className: "list list-6" },
+	{ linkName: "התנתקות", goTo: "/SignOut", className: "list list-7" },
 ];
 
 function Sidebar() {
-  const [path, setPath] = useState("");
-  const dispatch = useDispatch();
-  useEffect(() => {
-    if (window.location.pathname === "/Home") {
-      setPath("/Home");
-    }
-  }, []);
+	const [path, setPath] = useState("");
+	const dispatch = useDispatch();
+	useEffect(() => {
+		if (window.location.pathname === "/Home") {
+			setPath("/Home");
+		}
+	}, []);
 
-  return (
-    <aside className="aside-bar">
-      <Link onClick={() => setPath(links[0].goTo)} to={links[0].goTo}>
-        <li className={links[0].className}>
-          <HomePurpleIcon path={path} />
-          {links[0].linkName}
-        </li>
-      </Link>
-      <Link onClick={() => setPath(links[1].goTo)} to={links[1].goTo}>
-        <li className={links[1].className}>
-          <MessagesStockIcon path={path} />
-          {links[1].linkName}
-        </li>
-      </Link>
-      <Link onClick={() => setPath(links[2].goTo)} to={links[2].goTo}>
-        <li className={links[2].className}>
-          <GroupsIcon path={path} />
-          {links[2].linkName}
-        </li>
-      </Link>
-      <Link onClick={() => setPath(links[3].goTo)} to={links[3].goTo}>
-        <li className={links[3].className}>
-          <SendIcon path={path} />
-          {links[3].linkName}
-        </li>
-      </Link>
-      <Link onClick={() => setPath(links[4].goTo)} to={links[4].goTo}>
-        <li className={links[4].className}>
-          <HistoryIcon path={path} />
-          {links[4].linkName}
-        </li>
-      </Link>
-      <Link onClick={() => setPath(links[5].goTo)} to={links[5].goTo}>
-        <li className={links[5].className}>
-          <ProfileIcone path={path} />
-          {links[5].linkName}
-        </li>
-      </Link>
-      <Link
-        onClick={() =>
-          smalltalk
-            .confirm("", "אתה בטוח שברצונך להתנתק?")
-            .then(() => dispatch(addUserId("")))
-            .catch(() => {
-              return;
-            })
-        }
-      >
-        <li className={links[6].className}>
-          <SignOutIcon path={path} />
-          {links[6].linkName}
-        </li>
-      </Link>
-    </aside>
-  );
+	return (
+		<aside className="aside-bar">
+			<Link onClick={() => setPath(links[0].goTo)} to={links[0].goTo}>
+				<li className={links[0].className}>
+					<HomePurpleIcon path={path} />
+					{links[0].linkName}
+				</li>
+			</Link>
+			<Link onClick={() => setPath(links[1].goTo)} to={links[1].goTo}>
+				<li className={links[1].className}>
+					<MessagesStockIcon path={path} />
+					{links[1].linkName}
+				</li>
+			</Link>
+			<Link onClick={() => setPath(links[2].goTo)} to={links[2].goTo}>
+				<li className={links[2].className}>
+					<GroupsIcon path={path} />
+					{links[2].linkName}
+				</li>
+			</Link>
+			<Link onClick={() => setPath(links[3].goTo)} to={links[3].goTo}>
+				<li className={links[3].className}>
+					<SendIcon path={path} />
+					{links[3].linkName}
+				</li>
+			</Link>
+			<Link onClick={() => setPath(links[4].goTo)} to={links[4].goTo}>
+				<li className={links[4].className}>
+					<HistoryIcon path={path} />
+					{links[4].linkName}
+				</li>
+			</Link>
+			<Link onClick={() => setPath(links[5].goTo)} to={links[5].goTo}>
+				<li className={links[5].className}>
+					<ProfileIcone path={path} />
+					{links[5].linkName}
+				</li>
+			</Link>
+			<div
+				onClick={() =>
+					smalltalk
+						.confirm("", "אתה בטוח שברצונך להתנתק?")
+						.then(() => dispatch(addUserId("")))
+						.catch(() => {
+							return;
+						})
+				}
+			>
+				<li className={links[6].className}>
+					<SignOutIcon path={path} />
+					{links[6].linkName}
+				</li>
+			</div>
+		</aside>
+	);
 }
 
 export default Sidebar;
